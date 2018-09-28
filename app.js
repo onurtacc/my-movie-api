9898b995c1be2411fb1e12305b7a9a80e8e609fb
onurtacc <onur.tac@orgino.com.tr>
2018-09-28 15:28:16 +0300

    add jsonwebtoken

--- app.js
+++ app.js
@@ -10,8 +10,16 @@ const director = require('./routes/director');
 
 const app = express();
 
+
+// Middleware
+const verifyToken = require('./middleware/verify-token');
+
 //db connection
 const db = require('./helper/db')();
+
+//config
+const config = require('./config');
+app.set('api_secret_key', config.api_secret_key);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', verifyToken);
 app.use('/api/movies', movie);
 app.use('/api/directors', director);
 
